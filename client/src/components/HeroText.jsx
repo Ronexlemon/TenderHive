93ac8edd1e5361ce00b112a8bbc7f8e263764278
RonexLemon <ronexondimu@gmail.com>
2023-04-03 12:32:53 +0300

    change tittle

--- client/src/components/HeroText.jsx
+++ client/src/components/HeroText.jsx
@@ -6,13 +6,13 @@ const  HeroText = () => {
     <section className="flex flex-col items-center">
       <article className=" flex flex-col items-center text-white my-3 h-[400px] md:h-[300px] justify-around">
         <div className="text-5xl font-bold text-center pt-10">
-          Bringing trust and transparency to{" "}
+         Openness fosters trust and transparency in{" "}
           <div className="text-transparent bg-clip-text bg-gradient-to-r from-[#FE873F] to-[#FFC947]">
             procurement of services
           </div>
         </div>
         <h3 className="text-base text-[#fafafab5] w-[95%] xsm:w-[75%] md:w-[60%] text-center">
-        SupplySphere is a blockchain based application that makes it easy to
+        TenderHive is a blockchain based application that makes it easy to
           advertise and apply for tenders with the highest level of
           transparency.
         </h3>
